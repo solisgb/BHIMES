471afe3ac6102e310f0b92f8db58196248d8304c
Luis Solís <37953927+solisgb@users.noreply.github.com>
2020-06-22 22:17:47 +0200

    Add files via upload

--- bhimes.py
+++ bhimes.py
@@ -3,14 +3,45 @@
 Created on Sun Jun 21 12:51:28 2020
 
 @author: solis
+
+aquifer data
+    fid integer
+    xc real, x polygon centroid
+    yc real, y polygon centroid
+    y4326 real, latitude epgs 4326
+    area real, polygon area m2
+    name text(100), aquifer name
+
+outcrop data and parameters
+    fid integer,
+    aquifer integer, aquifer identifier
+    lito text(250), lithology
+    period text(100), geologic period, age
+    perme text(50), permeability mm/d
+    area real, polygon area m2
+    ia real, initial abstraccion capacity (mainly vegetation) mm
+    whc real, soil water holding capacity mm
+    kdirect real, direct permeability mm/d
+    kuz real, unsatured zone pemeability mm/d
+    klateral real, lateral permeability from whc mm/d
+    krunoff real, superficial runoff infiltration mm/d
+
+meteorologycal data related with aquifer -in this version-
+    fid integer
+    date text(10)
+    p real, precipitation in date mm
+    tmin real, minimum temperature Celsius deg
+    tmax real, minimum temperature Celsius deg
+    tavg real, average temperature Celsius deg
 """
+
 import littleLogging as logging
 
 
 class BHIMES():
     """
     Balance HIdroMEteorológico en el Suelo
-    Water balance in soil
+    Soil water balance
     """
 
     def __init__(self):
@@ -27,8 +58,11 @@ class BHIMES():
         """
         create table if not exists aquifer(
             fid integer,
+            xc real,
+            yc real,
+            y4326 real,
+            area real,
             name text(100),
-            sup_m2 real,
             primary key (fid)
         )
         """
@@ -36,31 +70,47 @@ class BHIMES():
         stm2 = \
         """
         create table if not exists outcrop(
-            fid integer,
+            fid integer primary key,
             aquifer integer,
             lito text(250),
             period text(100),
             perme text(50),
-            sup_m2 real,
+            area real,
             ia real,
             whc real,
             kdirect real,
             kuz real,
             klateral real,
             krunoff real,
-            primary key(fid)
-        );
-        references aquifer(fid)
-            on update cascade
-            on delete cascade;
-        create index outcrop_aquifer
-            on outcrop(aquifer);
+            foreign key (fid) references aquifer(fid)
+        )
+        """
+
+        stm3 = \
+        """
+        create index if not exists outcrop_aquifer
+            on outcrop(aquifer)
+        """
+
+        stm4 = \
+        """
+        create table if not exists met(
+            fid integer primary key,
+            date text(10)
+            p real,
+            tmin real,
+            tmax real,
+            tavg real,
+            foreign key (fid) references aquifer(fid)
+        )
         """
 
         con = sqlite3.connect(dst)
+        con.execute("PRAGMA encoding = 'utf-8'")
+        con.execute("PRAGMA foreign_keys = 1")
         cur = con.cursor()
-        cur.execute(stm1)
-        cur.execute(stm2)
+        for stm in (stm1, stm2, stm3, stm4):
+            cur.execute(stm)
         con.commit()
         con.close()
 
@@ -74,17 +124,17 @@ class BHIMES():
 
         select1 = \
         """
-        select * from aquifer where fid=?
+        select fid from aquifer where fid=?
         """
         update1 = \
         """
-        update aquifer set name=?, sup_m2=?
+        update aquifer set xc=?, yc=?, y4326=?, area=?, name=?
         where fid=?
         """
         insert1 = \
         """
-        insert into aquifer(fid, name, sup_m2)
-        values (?, ?, ?)
+        insert into aquifer(fid, xc, yc, y4326, area, name)
+        values (?, ?, ?, ?, ?, ?)
         """
 
         con = sqlite3.connect(db)
@@ -96,13 +146,16 @@ class BHIMES():
                     continue
                 words = line.strip().split(separator)
                 fid = int(words[0])
-                name = words[1]
-                sup = float(words[2])
+                xc = float(words[1])
+                yc = float(words[2])
+                y4326 = float(words[3])
+                area = float(words[4])
+                name = words[5]
                 row = cur.execute(select1, (fid,)).fetchone()
                 if row:
-                    cur.execute(update1, (name, sup, fid))
+                    cur.execute(update1, (xc, yc, y4326, area, name, fid))
                 else:
-                    cur.execute(insert1, (fid, name, sup))
+                    cur.execute(insert1, (fid, xc, yc, y4326, area, name))
 
         con.commit()
         con.close()
@@ -117,19 +170,19 @@ class BHIMES():
 
         select1 = \
         """
-        select * from outcrop where fid=?
+        select fid from outcrop where fid=?
         """
         update1 = \
         """
-        update outcrop set aquifer=?, lito=?, period=?, perme=?, sup_m2=?,
+        update outcrop set aquifer=?, lito=?, period=?, perme=?, area=?,
         ia=?, whc=?, kdirect=?, kuz=?, klateral=?, krunoff=?
         where fid=?
         """
         insert1 = \
         """
-        insert into outcrop(fid, aquifer, lito, period, perme, sup_m2, ia, whc,
+        insert into outcrop(fid, aquifer, lito, period, perme, area, ia, whc,
             kdirect, kuz, klateral, krunoff)
-        values (?,?,?,?,?,?,?,?,?,?,?,?,?)
+        values (?,?,?,?,?,?,?,?,?,?,?,?)
         """
 
         con = sqlite3.connect(db)
@@ -145,7 +198,7 @@ class BHIMES():
                 lito = words[2]
                 period = words[3]
                 perme = words[4]
-                sup_m2 = float(words[5])
+                area = float(words[5])
                 ia = float(words[6])
                 whc = float(words[7])
                 kdirect = float(words[8])
@@ -155,12 +208,70 @@ class BHIMES():
                 row = cur.execute(select1, (fid,)).fetchone()
                 if row:
                     cur.execute(update1, (acu, lito, period, perme,
-                                          sup_m2, ia, whc, kdirect, kuz,
+                                          area, ia, whc, kdirect, kuz,
                                           klateral, krunoff, fid))
                 else:
                     cur.execute(insert1, (fid, acu, lito, period, perme,
-                                          sup_m2, ia, whc, kdirect, kuz,
+                                          area, ia, whc, kdirect, kuz,
                                           klateral, krunoff))
 
         con.commit()
         con.close()
+
+
+    def met_upsert_from_file01(self, db: str, org: str,
+                                 nskip: int=1, separator: str=';'):
+        """
+        inserts or update meteorological data from text file -utf8-
+        with no average temperature
+        """
+        import sqlite3
+
+        select1 = \
+        """
+        select fid from met where fid=?
+        """
+        update1 = \
+        """
+        update met set date=?, p=?, tmin=?, tmax=?, tavg=?
+        where fid=?
+        """
+        insert1 = \
+        """
+        insert into aquifer(fid, date, p, tmin, tmax, tavg)
+        values (?, ?, ?, ?, ?, ?)
+        """
+
+        con = sqlite3.connect(db)
+        cur = con.cursor()
+
+        with open(org, 'r') as f:
+            for i, line in enumerate(f):
+                if i < nskip:
+                    continue
+                words = line.strip().split(separator)
+                fid = words[0]
+                date = words[1]
+                p = float(words[2]) / 10.  # dmm -> mm
+                tmin = float(words[3]) / 10.  # dºC -> ºC
+                tmax = float(words[4]) / 10.
+                tavg = (tmin + tmax) / 2.
+
+                row = cur.execute(select1, (fid,)).fetchone()
+                if row:
+                    cur.execute(update1, (date, p, tmin, tmax, tavg, fid))
+                else:
+                    cur.execute(insert1, (fid, date, p, tmin, tmax, tavg))
+
+        con.commit()
+        con.close()
+
+
+    def swb01(self, aquifers: list, table_output: str='swb01'):
+        """
+        soil water balance version 0.01
+        """
+        select1 = \
+        """
+
+        """
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ Created on Sun Jun 21 12:50:31 2020
 import littleLogging as logging
 
 db = r'H:\off\balan\ch_202006.db'
-org = r'H:\off\balan\acuiferos.txt'
+org1 = r'H:\off\balan\acuiferos.txt'
+org2 = r'H:\off\balan\afloramientos.txt'
+org3 = r'H:\off\balan\met_centroids.txt'
 
 if __name__ == "__main__":
 
@@ -23,7 +25,9 @@ if __name__ == "__main__":
 
         b = BHIMES()
         b.create_db(db)
-        b.acu_upsert_from_file(db, org, separator='\t')
+        b.aquifer_upsert_from_file(db, org1, separator='\t')
+        b.outcrop_upsert_from_file(db, org2, separator='\t')
+        b.met_upsert_from_file01(db, org3, separator='\t')
 
         xtime = time() - startTime
         print(f'El script tardó {xtime:0.1f} s')
@@ -33,7 +37,7 @@ if __name__ == "__main__":
         logging.append(f'ValueError exception\n{msg}')
     except ImportError:
         msg = traceback.format_exc()
-        print (f'ImportError exception\n{msg}')
+        logging.append(f'ImportError exception\n{msg}')
     except Exception:
         msg = traceback.format_exc()
         logging.append(f'Exception\n{msg}')
